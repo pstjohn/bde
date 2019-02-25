fc418fa0413e327735c87e765fd8ac77f743e7f7
Peter St. John <peterc.stjohn@gmail.com>
2019-02-25 10:58:52 -0700

    gaussian edits

--- bde/gaussian.py
+++ bde/gaussian.py
@@ -37,6 +37,8 @@ def optimize_molecule_mmff(smiles, max_conformers=1000):
 
 
 def write_gaussian_input_file(mol, confId, cid, scratchdir='/scratch/pstjohn'):
+    """ Given an rdkit.Mol object with an optimized, minimum energy conformer
+    ID, write a gaussian input file using openbabel to the scratch folder """
 
     input_file = scratchdir + '/bde/gjf/{}_0.gjf'.format(cid)
     checkpoint_file = scratchdir + '/bde/chk/{0}_0.chk'.format(cid)
@@ -79,6 +81,7 @@ def write_gaussian_input_file(mol, confId, cid, scratchdir='/scratch/pstjohn'):
 
 
 def run_gaussian(gjf, cid, scratchdir='/scratch/pstjohn'):
+    """ Run the given Guassian input file (with associated mol ID) """
 
     output_file = scratchdir + '/bde/log/{}_0.log'.format(cid)
     
@@ -87,5 +90,52 @@ def run_gaussian(gjf, cid, scratchdir='/scratch/pstjohn'):
         env['GAUSS_SCRDIR'] = tmpdirname
         subprocess.call("module load gaussian/G16B && g16 < {0} > {1}".format(
             gjf, output_file), shell=True, env=env)
+
+    return output_file
+
+
+def parse_log_file(logfile, smiles, cid):
+    """ Parse the gaussian log file using cclib, return the optimized mol and
+    enthalpy. """
+
+    # Parse the output log with cclib, assert the optimization completed
+    logging.disable(logging.INFO)
+    data = cclib.io.ccread(logfile)
+    assert data.optdone, "Optimization not converged"
     
+    if hasattr(data, 'vibfreqs'): # single atoms don't have this property
+        assert min(data.vibfreqs) >= 0, "Imaginary Frequency"
+
+    # Create an RDKit Molecule from the SMILES string
+    mol = Chem.MolFromSmiles(smiles)
+    mol = AllChem.AddHs(mol)
+    AllChem.EmbedMolecule(mol)
+    conf = mol.GetConformer()
     
+    assert np.allclose(
+        np.array([a.GetAtomicNum() for a in mol.GetAtoms()]),
+        data.atomnos), "Stoichiometry check failed"
+
+    # Correct the 3D positions of the atoms using the optimized geometry
+    for i in range(conf.GetNumAtoms()):
+        conf.SetAtomPosition(i, data.atomcoords[-1][i])
+    
+    covalent_radii = {'H': .31, 'C': .76, 'N': .71, 'O': .66}
+    
+    # Check bond lengths
+    for bond in mol.GetBonds():
+        length = GetBondLength(
+            mol.GetConformer(), bond.GetBeginAtomIdx(), bond.GetEndAtomIdx())
+        
+        max_length = (covalent_radii[bond.GetBeginAtom().GetSymbol()] + 
+                      covalent_radii[bond.GetEndAtom().GetSymbol()] + 0.4)
+        
+        assert length <= max_length, "bond greater than maximum covalent length"
+
+
+    # Set property fields of the molecule for final SDF export
+    mol.SetProp("_Name", str(cid))
+    mol.SetProp('SMILES', smiles)
+    mol.SetDoubleProp('Enthalpy', data.enthalpy)
+
+    return mol, data.enthalpy
