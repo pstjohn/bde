5cb4258ef6ee522926f3a56e02a0c961243eac59
Peter St. John <peterc.stjohn@gmail.com>
2019-03-21 12:42:56 -0600

    restructring in class

--- bde/gaussian.py
+++ bde/gaussian.py
@@ -3,6 +3,7 @@ import tempfile
 import subprocess
 import logging
 import uuid
+import time
 
 import numpy as np
 import cclib
@@ -13,191 +14,222 @@ from rdkit.Chem.rdMolTransforms import GetBondLength
 
 logging.getLogger("cclib").setLevel(30)
 
-def optimize_molecule_mmff(smiles, max_conformers=1000, min_conformers=100):
-    """ Embed a molecule in 3D space, optimizing a number of conformers and
-    selecting the most stable
-    """
-    
-    mol = Chem.MolFromSmiles(smiles)
-    mol = Chem.rdmolops.AddHs(mol)
 
-    # Use min < 3^n < max conformers, where n is the number of rotatable bonds
-    NumRotatableBonds = AllChem.CalcNumRotatableBonds(mol)
-    NumConformers = np.clip(3**NumRotatableBonds, min_conformers, max_conformers)
+class GaussianRunner(object):
 
-    conformers = AllChem.EmbedMultipleConfs(
-        mol, numConfs=int(NumConformers), pruneRmsThresh=0.2, randomSeed=1,
-        useExpTorsionAnglePrefs=True, useBasicKnowledge=True)
+    def __init__(self, smiles, cid, type_, max_conformers=1000,
+                 min_conformers=100, nprocs=18, mem='40GB',
+                 scratchdir='/tmp/scratch',
+                 projectdir='/projects/cooptimasoot/psj_bde/'):
+        """ Class to handle the overall temporary directory management for
+        running Gaussian on Eagle """
 
-    def optimize_conformer(conformer):
-        prop = AllChem.MMFFGetMoleculeProperties(mol, mmffVariant="MMFF94s")
-        ff = AllChem.MMFFGetMoleculeForceField(mol, prop, confId=conformer)
-        ff.Minimize()
-        return float(ff.CalcEnergy())
+        self.smiles = smiles
+        self.cid = cid
+        self.type_ = type_
+        self.max_conformers = max_conformers
+        self.min_conformers = min_conformers
+        self.nprocs = nprocs
+        self.mem = mem
+        self.scratchdir = scratchdir
+        self.projectdir = projectdir
 
-    assert conformers, "Conformer embedding failed"
 
-    if len(conformers) == 1:
-        logging.critical(
-            'Only 1 conformer for SMILES {}'.format(smiles))
-        most_stable_conformer = conformers[0]
+    def process():
+
+        with tempfile.TemporaryDirectory(dir=self.scratchdir) as tmpdirname:
+
+            print("starting SMILES {}".format(self.smiles))
+            mol, confId = self.optimize_molecule_mmff(self.smiles)
+            self.write_gaussian_input_file(mol, confId, tmpdirname)
+
+            # Run gaussian and time calculation
+            gauss_start_time = time.time()
+            self.run_gaussian(tmpdirname)
+            gauss_run_time = time.time() - gauss_start_time
+            print("python walltime for SMILES {0}: {1}".format(
+                self.smiles, gauss_run_time))
+
+
+            mol, enthalpy = self.parse_log_file()
+            log = self.cleanup()
+            
+            molstr = Chem.MolToMolBlock(mol)
+
+            return molstr, enthalpy, log
+
+
+    def optimize_molecule_mmff(self):
+        """ Embed a molecule in 3D space, optimizing a number of conformers and
+        selecting the most stable
+        """
         
-    else:
-        conformer_energies = np.array(
-            [optimize_conformer(conformer) for conformer in conformers])
-        most_stable_conformer = conformer_energies.argmin()
+        mol = Chem.MolFromSmiles(smiles)
+        mol = Chem.rdmolops.AddHs(mol)
 
-    return mol, int(most_stable_conformer)
+        # Use min < 3^n < max conformers, where n is the number of rotatable bonds
+        NumRotatableBonds = AllChem.CalcNumRotatableBonds(mol)
+        NumConformers = np.clip(3**NumRotatableBonds, min_conformers, max_conformers)
+
+        conformers = AllChem.EmbedMultipleConfs(
+            mol, numConfs=int(NumConformers), pruneRmsThresh=0.2, randomSeed=1,
+            useExpTorsionAnglePrefs=True, useBasicKnowledge=True)
+
+        def optimize_conformer(conformer):
+            prop = AllChem.MMFFGetMoleculeProperties(mol, mmffVariant="MMFF94s")
+            ff = AllChem.MMFFGetMoleculeForceField(mol, prop, confId=conformer)
+            ff.Minimize()
+            return float(ff.CalcEnergy())
+
+        assert conformers, "Conformer embedding failed"
+
+        if len(conformers) == 1:
+            logging.critical(
+                'Only 1 conformer for SMILES {}'.format(smiles))
+            most_stable_conformer = conformers[0]
+            
+        else:
+            conformer_energies = np.array(
+                [optimize_conformer(conformer) for conformer in conformers])
+            most_stable_conformer = conformer_energies.argmin()
+
+        return mol, int(most_stable_conformer)
 
 
-def write_gaussian_input_file(mol, confId, cid, scratchdir='/tmp/scratch/pstjohn',
-                              nprocs=18, mem='24GB'):
-    """ Given an rdkit.Mol object with an optimized, minimum energy conformer
-    ID, write a gaussian input file using openbabel to the scratch folder """
+    def write_gaussian_input_file(self, mol, confId, tmpdirname):
+        """ Given an rdkit.Mol object with an optimized, minimum energy conformer
+        ID, write a gaussian input file using openbabel to the scratch folder """
 
-    run_hex = uuid.uuid4().hex[:6]
-    input_file = scratchdir + '/bde/gjf/{0}_{1}.gjf'.format(cid, run_hex)
-    checkpoint_file = scratchdir + '/bde/chk/{0}_{1}.chk'.format(cid, run_hex)
-    
-    header1 = [
-        '%chk={0}'.format(checkpoint_file),        
-        '%MEM={}'.format(mem),
-        '%nprocshared={}'.format(nprocs),
-        '# stable=opt M062X/Def2TZVP scf=(xqc,maxconventionalcycles=400)'
-        ' nosymm guess=mix']
-    
-    with tempfile.NamedTemporaryFile(
-            'w', suffix='.sdf', dir=scratchdir + '/gauss_scr') as sdf_file:
-        writer = Chem.SDWriter(sdf_file)
-        mol.SetProp('_Name', str(cid))
-        writer.write(mol, confId=confId)
-        writer.close()
+        self.run_hex = uuid.uuid4().hex[:6]
+        self.input_file = tmpdirname + '/{0}_{1}.gjf'.format(cid, self.run_hex)
+        checkpoint_file = tmpdirname + '/{0}_{1}.chk'.format(cid, self.run_hex)
 
-        subprocess.call(
-            ['obabel', sdf_file.name, '-O', input_file, '-xk', '\n'.join(header1)])
-
+        if self.type_ is 'fragment':
+            # Run stable=opt
         
-    with open(input_file, 'r') as f:
-        chg_mul = f.readlines()[7]
+            header1 = [
+                '%chk={0}'.format(checkpoint_file),        
+                '%MEM={}'.format(mem),
+                '%nprocshared={}'.format(nprocs),
+                '# stable=opt M062X/Def2TZVP scf=(xqc,maxconventionalcycles=400)'
+                ' nosymm guess=mix']
+            
+            with tempfile.NamedTemporaryFile(
+                    'w', suffix='.sdf', dir=scratchdir + '/gauss_scr') as sdf_file:
+                writer = Chem.SDWriter(sdf_file)
+                mol.SetProp('_Name', str(cid))
+                writer.write(mol, confId=confId)
+                writer.close()
 
-    with open(input_file, 'a') as f:
+                subprocess.call(
+                    ['obabel', sdf_file.name, '-O', input_file, '-xk',
+                     '\n'.join(header1)])
+
+                
+            with open(input_file, 'r') as f:
+                chg_mul = f.readlines()[7]
+
+            with open(input_file, 'a') as f:
+                
+                header2 = [
+                    '--link1--',
+                    '%chk={0}'.format(checkpoint_file),        
+                    '%MEM={}'.format(mem),
+                    '%nprocshared={}'.format(nprocs),
+                    '# opt freq M062X/Def2TZVP scf=(xqc,maxconventionalcycles=400)'
+                        ' nosymm guess=read geom=check\n',
+                    ' {}\n'.format(mol.GetProp('_Name')),
+                    chg_mul
+                ]
+                
+                f.write('\n'.join(header2))
+
+        else:
+
+            header1 = [
+                '%MEM={}'.format(mem),
+                '%nprocshared={}'.format(nprocs),
+                '# opt freq M062X/Def2TZVP scf=(xqc,maxconventionalcycles=400) nosymm']
+            
+            with tempfile.NamedTemporaryFile(
+                    'w', suffix='.sdf', dir=scratchdir + '/gauss_scr') as sdf_file:
+                writer = Chem.SDWriter(sdf_file)
+                mol.SetProp('_Name', str(cid))
+                writer.write(mol, confId=confId)
+                writer.close()
+
+                subprocess.call(
+                    ['obabel', sdf_file.name, '-O', input_file, '-xk', '\n'.join(header1)])
+            
+
+    def run_gaussian(self, tmpdirname):
+        """ Run the given Guassian input file (with associated mol ID) """
+
+        self.log = tmpdirname + '{0}_{1}.log'.format(self.cid, self.run_hex)
         
-        header2 = [
-            '--link1--',
-            '%chk={0}'.format(checkpoint_file),        
-            '%MEM={}'.format(mem),
-            '%nprocshared={}'.format(nprocs),
-            '# opt freq M062X/Def2TZVP scf=(xqc,maxconventionalcycles=400)'
-                ' nosymm guess=read geom=check\n',
-            ' {}\n'.format(mol.GetProp('_Name')),
-            chg_mul
-        ]
+        with tempfile.TemporaryDirectory(dir=tmpdirname) as gausstmp:
+            env = os.environ.copy()
+            env['GAUSS_SCRDIR'] = gausstmp
+            subprocess.call("module load gaussian/G16B && g16 < {0} > {1}".format(
+                self.gjf, self.log), shell=True, env=env)
+
+
+    def parse_log_file(self):
+        """ Parse the gaussian log file using cclib, return the optimized mol and
+        enthalpy. """
+
+        # Parse the output log with cclib, assert the optimization completed
+        data = cclib.io.ccread(self.log)
+        assert data.optdone, "Optimization not converged"
         
-        f.write('\n'.join(header2))
+        if hasattr(data, 'vibfreqs'): # single atoms don't have this property
+            assert min(data.vibfreqs) >= 0, "Imaginary Frequency"
+
+        # Create an RDKit Molecule from the SMILES string
+        mol = Chem.MolFromSmiles(smiles)
+        mol = AllChem.AddHs(mol)
+        AllChem.EmbedMolecule(mol)
+        conf = mol.GetConformer()
         
-    return input_file, run_hex
+        assert np.allclose(
+            np.array([a.GetAtomicNum() for a in mol.GetAtoms()]),
+            data.atomnos), "Stoichiometry check failed"
 
-
-def run_gaussian(gjf, cid, run_hex, scratchdir='/tmp/scratch/pstjohn'):
-    """ Run the given Guassian input file (with associated mol ID) """
-
-    output_file = scratchdir + '/bde/log/{0}_{1}.log'.format(cid, run_hex)
-    
-    with tempfile.TemporaryDirectory(dir=scratchdir + '/gauss_scr') as tmpdirname:
-        env = os.environ.copy()
-        env['GAUSS_SCRDIR'] = tmpdirname
-        subprocess.call("module load gaussian/G16B && g16 < {0} > {1}".format(
-            gjf, output_file), shell=True, env=env)
-
-    return output_file
-
-
-def write_gaussian_input_file_parent(
-        mol, confId, cid, scratchdir='/tmp/scratch/pstjohn', nprocs=18,
-        mem='24GB'):
-    """ Given an rdkit.Mol object with an optimized, minimum energy conformer
-    ID, write a gaussian input file using openbabel to the scratch folder 
-    
-    This version does not check for a stable wavefunction.
-    """
-
-    run_hex = uuid.uuid4().hex[:6]
-    input_file = scratchdir + '/bde/gjf/{0}_{1}.gjf'.format(cid, run_hex)
-    
-    header1 = [
-        '%MEM={}'.format(mem),
-        '%nprocshared={}'.format(nprocs),
-        '# opt freq M062X/Def2TZVP scf=(xqc,maxconventionalcycles=400) nosymm']
-    
-    with tempfile.NamedTemporaryFile(
-            'w', suffix='.sdf', dir=scratchdir + '/gauss_scr') as sdf_file:
-        writer = Chem.SDWriter(sdf_file)
-        mol.SetProp('_Name', str(cid))
-        writer.write(mol, confId=confId)
-        writer.close()
-
-        subprocess.call(
-            ['obabel', sdf_file.name, '-O', input_file, '-xk', '\n'.join(header1)])
-
-    return input_file, run_hex
-
-
-def parse_log_file(logfile, smiles, cid):
-    """ Parse the gaussian log file using cclib, return the optimized mol and
-    enthalpy. """
-
-    # Parse the output log with cclib, assert the optimization completed
-    data = cclib.io.ccread(logfile)
-    assert data.optdone, "Optimization not converged"
-    
-    if hasattr(data, 'vibfreqs'): # single atoms don't have this property
-        assert min(data.vibfreqs) >= 0, "Imaginary Frequency"
-
-    # Create an RDKit Molecule from the SMILES string
-    mol = Chem.MolFromSmiles(smiles)
-    mol = AllChem.AddHs(mol)
-    AllChem.EmbedMolecule(mol)
-    conf = mol.GetConformer()
-    
-    assert np.allclose(
-        np.array([a.GetAtomicNum() for a in mol.GetAtoms()]),
-        data.atomnos), "Stoichiometry check failed"
-
-    # Correct the 3D positions of the atoms using the optimized geometry
-    for i in range(conf.GetNumAtoms()):
-        conf.SetAtomPosition(i, data.atomcoords[-1][i])
-    
-    covalent_radii = {'H': .31, 'C': .76, 'N': .71, 'O': .66}
-    
-    # Check bond lengths
-    for bond in mol.GetBonds():
-        length = GetBondLength(
-            mol.GetConformer(), bond.GetBeginAtomIdx(), bond.GetEndAtomIdx())
+        # Correct the 3D positions of the atoms using the optimized geometry
+        for i in range(conf.GetNumAtoms()):
+            conf.SetAtomPosition(i, data.atomcoords[-1][i])
         
-        max_length = (covalent_radii[bond.GetBeginAtom().GetSymbol()] + 
-                      covalent_radii[bond.GetEndAtom().GetSymbol()] + 0.4)
+        covalent_radii = {'H': .31, 'C': .76, 'N': .71, 'O': .66}
         
-        assert length <= max_length, "bond greater than maximum covalent length"
+        # Check bond lengths
+        for bond in mol.GetBonds():
+            length = GetBondLength(
+                mol.GetConformer(), bond.GetBeginAtomIdx(), bond.GetEndAtomIdx())
+            
+            max_length = (covalent_radii[bond.GetBeginAtom().GetSymbol()] + 
+                          covalent_radii[bond.GetEndAtom().GetSymbol()] + 0.4)
+            
+            assert length <= max_length, "bond greater than maximum covalent length"
 
 
-    # Set property fields of the molecule for final SDF export
-    mol.SetProp("_Name", str(cid))
-    mol.SetProp('SMILES', smiles)
-    mol.SetDoubleProp('Enthalpy', data.enthalpy)
+        # Set property fields of the molecule for final SDF export
+        mol.SetProp("_Name", str(cid))
+        mol.SetProp('SMILES', smiles)
+        mol.SetDoubleProp('Enthalpy', data.enthalpy)
 
-    return mol, data.enthalpy
+        return mol, data.enthalpy
 
-def cleanup(log, gjf, projectdir='/projects/cooptimasoot/psj_bde/'):
-    """ Compress files and store in /projects """
+    def cleanup(self):
+        """ Compress files and store in /projects """
 
-    log_basename = os.path.basename(log)
-    gjf_basename = os.path.basename(gjf)
+        log_basename = os.path.basename(self.log)
+        gjf_basename = os.path.basename(self.gjf)
 
-    newlog = projectdir + log_basename + '.gz'
-    newgjf = projectdir + gjf_basename + '.gz'
+        newlog = self.projectdir + log_basename + '.gz'
+        newgjf = self.projectdir + gjf_basename + '.gz'
 
-    subprocess.call(['gzip', log, gjf])
-    subprocess.call(['mv', log + '.gz', newlog])
-    subprocess.call(['mv', gjf + '.gz', newgjf])
+        subprocess.call(['gzip', self.log, gjf])
+        subprocess.call(['mv', self.log + '.gz', newlog])
+        subprocess.call(['mv', self.gjf + '.gz', newgjf])
 
-    return newlog, newgjf
+        return newlog
